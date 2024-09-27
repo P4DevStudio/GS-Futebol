d1ad0c1b97bd668e22b7925664dcc7d230e52a51
Luigui Paccine <paccine.dev@gmail.com>
2024-09-27 01:37:44 -0300

    quase funcionando.

--- painel/central/static/js/sidebar.js
+++ painel/central/static/js/sidebar.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sidebar = document.getElementById('sidebar');
+    const sidebar = document.querySelector('.vertical-menu');
     const navbar = document.querySelector('.navbar-header');
     const content = document.querySelector('.main-content');
     const toggleButton = document.getElementById('navbar-menu-toggle');
 
     if (toggleButton) {
         toggleButton.addEventListener('click', function() {
-            sidebar.classList.toggle('minimized'); // Alterna a classe minimizada da sidebar
-            navbar.classList.toggle('sidebar-minimized'); // Ajusta a navbar
-            content.classList.toggle('sidebar-minimized'); // Ajusta o conteúdo
+            // Alterna entre a versão minimizada e expandida no desktop
+            sidebar.classList.toggle('minimized');
+            navbar.classList.toggle('sidebar-minimized');
+            content.classList.toggle('sidebar-minimized');
 
-            // Exibe ou oculta a sidebar no mobile ao clicar no botão
+            // No mobile, a sidebar aparecerá sobre o conteúdo ao clicar
             if (window.innerWidth <= 768) {
-                sidebar.classList.toggle('active'); // Mostra a sidebar no mobile
+                sidebar.classList.toggle('active'); // Aplica classe 'active' para mostrar no mobile
             }
         });
     }
